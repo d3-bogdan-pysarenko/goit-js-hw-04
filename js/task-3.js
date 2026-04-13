2bbfee7208c259d16a43af5fa2c9fe204765221c
Bogdan Pysarenko <bogdan.pysarenko@door3.com>
2026-04-13 17:06:31 +0300

    hw 4 updates

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,26 @@
-const filterArray = function (numbers, value) {
-  const newArr = [];
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
 
-  for (const num of numbers) {
-    if (num > value) {
-      newArr.push(num);
-    }
-  }
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
 
-  return newArr;
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 };
 
 // checking
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
