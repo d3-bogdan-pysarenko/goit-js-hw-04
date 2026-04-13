860d836994efbc1806b0fb15abd447f8a186c893
Bogdan <53533112+d3-bogdan-pysarenko@users.noreply.github.com>
2026-04-13 16:57:48 +0300

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+const makeArray = function (firstArray, secondArray, maxLength) {
+  const finalArr = firstArray.concat(secondArray);
+
+  if (finalArr.length > maxLength) {
+    return finalArr.slice(0, maxLength);
+  }
+
+  return finalArr;
+};
+
+// Checking
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
