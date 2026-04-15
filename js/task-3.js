4701bbbddb3be1247dc61a8252d31b8872a1665f
Bogdan Pysarenko <bogdan.pysarenko@door3.com>
2026-04-15 21:20:54 +0300

    task 3 upd

--- js/task-3.js
+++ js/task-3.js
@@ -11,7 +11,7 @@ const profile = {
   },
 
   updatePlayTime(hours) {
-    this.playTime += hours;
+    this.playTime = this.playTime + hours;
   },
 };
 
@@ -19,7 +19,7 @@ const profile = {
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-profile.changeUsername('Marco');
+profile.changeUsername("Marco");
 console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
